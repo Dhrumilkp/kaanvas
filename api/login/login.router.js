4e077f8938a4950e2b4bfec969c2965f605193c5
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-04-19 10:56:05 +0530

    Update

--- api/login/login.router.js
+++ api/login/login.router.js
@@ -1,5 +1,12 @@
 const { login } = require('./login.controller');
 const router = require("express").Router();
+const rateLimit = require("express-rate-limit");
 
-router.post("/",login);
+const LoginrateLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour window
+    max: 10, // start blocking after 5 requests
+    message: "Too many login attemps, please try after sometime"
+});
+
+router.post("/",LoginrateLimiter,login);
 module.exports = router;
