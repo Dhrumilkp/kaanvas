6b1c0deac35f09c5a970a9c1e2ed1f834769bbee
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-04-23 18:24:56 +0530

    Update

--- api/user/user.router.js
+++ api/user/user.router.js
@@ -3,11 +3,5 @@ const router = require("express").Router();
 const rateLimit = require("express-rate-limit");
 const {checkToken} = require("../../auth/token_validation");
 
-const LoginrateLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour window
-    max: 10, // start blocking after 5 requests
-    message: "Too many login attemps, please try after sometime"
-});
-
-router.get("/:id",checkToken,LoginrateLimiter,GetUser);
+router.get("/:id",checkToken,GetUser);
 module.exports = router;
