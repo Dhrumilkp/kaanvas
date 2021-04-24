cebfc98d90e75c91584179e2c5d3c17244522bdc
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-04-24 13:51:09 +0530

    UpdateUpdate

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -3,7 +3,7 @@ const pool = require("../../config/database");
 module.exports = {
     getUserByid : (id,callback) => {
         pool.query(
-            `SELECT u_firstname,u_lastname,u_email,u_username,onboarding_status,mailverify_status,u_join,u_profilepic_webp,u_profilepic_jpeg,u_coverpic_webp,u_coverpic_jpeg,u_verified,u_country,u_city,is_profile_complete,u_level,is_pro,customer_id,u_website_url FROM ka_user WHERE id = ?`,
+            `SELECT u_firstname,u_lastname,u_email,u_username,onboarding_status,mailverify_status,u_join,u_profilepic_webp,u_profilepic_jpeg,u_coverpic_webp,u_coverpic_jpeg,u_verified,u_country,u_city,is_profile_complete,u_level,is_pro,customer_id,u_website_url,u_dribbble_url,u_github_url FROM ka_user WHERE id = ?`,
             [
                 id
             ],
