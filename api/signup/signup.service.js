be620c4247946f1ef2f91131604590a96956f830
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-05-01 13:31:37 +0530

    Update

--- api/signup/signup.service.js
+++ api/signup/signup.service.js
@@ -74,108 +74,29 @@ module.exports = {
                             }
                         }
                     )
-                    if(data.login_type == "Google")
-                    {
-                        stripe.customers.create({
-                            description : 'RateFreelancer customer',
-                            name        : ''+data.u_firstname+' '+data.u_lastname+'',
-                            email       : data.u_email
-                        })
-                        .then(customer => {
-                            // Update the customer id of the user
-                            pool.query(
-                                `UPDATE ka_user SET customer_id = ? WHERE u_email = ?`,
-                                [
-                                    customer.id,
-                                    data.u_email
-                                ],
-                                (error,results,fields) => {
-                                    if(error)
-                                    {
-                                        console.log(error);
-                                    }
-                                }
-                            )
-                        })
-                        .catch(error => console.error(error));
-                        return callback(null,results)
-                    }
-                    else
-                    {
-                        // generate a token
-                        var secret = speakeasy.generateSecret({length: 20});
-                        var otp = speakeasy.totp({
-                            secret: secret.base32,
-                            encoding: 'base32',
-                            digits:4,
-                            step: 60,
-                            window:10
-                        });
+                    stripe.customers.create({
+                        description : 'RateFreelancer customer',
+                        name        : ''+data.u_firstname+' '+data.u_lastname+'',
+                        email       : data.u_email
+                    })
+                    .then(customer => {
+                        // Update the customer id of the user
                         pool.query(
-                            `insert into ka_emailvalidate(u_email,u_otp)
-                            value(?,?)`,
+                            `UPDATE ka_user SET customer_id = ? WHERE u_email = ?`,
                             [
-                                data.u_email,
-                                otp
+                                customer.id,
+                                data.u_email
                             ],
                             (error,results,fields) => {
                                 if(error)
                                 {
-                                    callback(error)
+                                    console.log(error);
                                 }
-                                // Send email with email template
-                                const request = mailjet
-                                .post("send", {'version': 'v3.1'})
-                                .request({
-                                    "Messages":[{
-                                        "From": {
-                                            "Email": "security-noreply@ratefreelancer.com",
-                                            "Name": "Ratefreelancer Security"
-                                        },
-                                        "To": [{
-                                            "Email": data.u_email,
-                                            "Name": data.u_firstname +' '+ data.u_lastname
-                                        }],
-                                        "Subject": otp +" is your verification otp for ratefreelancer",
-                                        "TextPart": "Hi "+data.u_firstname+" "+data.u_lastname+" "+otp+" is your verification otp use this in the next 6 minutes",
-                                        "HTMLPart": "<h1 style='font-color:#343a40;'>Welcome To RateFreelancer</h1><br>your verification otp is <b>"+otp+"</b>, use this otp to verify your email <br><br/> Cheers,<br>Ratefreelancer Team"
-                                    }]
-                                })
-                                request
-                                    .then((result) => {
-                                        // create customer in stripe
-                                        let stripe_customer_id;
-                                        stripe.customers.create({
-                                            description : 'RateFreelancer customer',
-                                            name        : ''+data.u_firstname+' '+data.u_lastname+'',
-                                            email       : data.u_email
-                                        })
-                                        .then(customer => {
-                                            // Update the customer id of the user
-                                            pool.query(
-                                                `UPDATE ka_user SET customer_id = ? WHERE u_email = ?`,
-                                                [
-                                                    customer.id,
-                                                    data.u_email
-                                                ],
-                                                (error,results,fields) => {
-                                                    if(error)
-                                                    {
-                                                        console.log(error);
-                                                    }
-                                                }
-                                            )
-                                        })
-                                        .catch(error => console.error(error));
-                                    })
-                                    .catch((err) => {
-                                        console.log(err);
-                                    })
+                                return callback(null,results);
                             }
-                        );
-                        return callback(null,results)
-                    }
-    
+                        )
+                    })
+                    .catch(error => console.error(error));
                 }
             );
         }
@@ -237,109 +158,79 @@ module.exports = {
                                 console.log(error);
                             }
                         }
-                    )
-                    if(data.login_type == "Google")
-                    {
-                        stripe.customers.create({
-                            description : 'RateFreelancer customer',
-                            name        : ''+data.u_firstname+' '+data.u_lastname+'',
-                            email       : data.u_email
-                        })
-                        .then(customer => {
-                            // Update the customer id of the user
-                            pool.query(
-                                `UPDATE ka_user SET customer_id = ? WHERE u_email = ?`,
-                                [
-                                    customer.id,
-                                    data.u_email
-                                ],
-                                (error,results,fields) => {
-                                    if(error)
-                                    {
-                                        console.log(error);
-                                    }
-                                }
-                            )
-                        })
-                        .catch(error => console.error(error));
-                        return callback(null,results)
-                    }
-                    else
-                    {
-                        // generate a token
-                        var secret = speakeasy.generateSecret({length: 20});
-                        var otp = speakeasy.totp({
-                            secret: secret.base32,
-                            encoding: 'base32',
-                            digits:4,
-                            step: 60,
-                            window:10
-                        });
-                        pool.query(
-                            `insert into ka_emailvalidate(u_email,u_otp)
-                            value(?,?)`,
-                            [
-                                data.u_email,
-                                otp
-                            ],
-                            (error,results,fields) => {
-                                if(error)
-                                {
-                                    callback(error)
-                                }
-                                // Send email with email template
-                                const request = mailjet
-                                .post("send", {'version': 'v3.1'})
-                                .request({
-                                    "Messages":[{
-                                        "From": {
-                                            "Email": "security-noreply@ratefreelancer.com",
-                                            "Name": "Ratefreelancer Security"
-                                        },
-                                        "To": [{
-                                            "Email": data.u_email,
-                                            "Name": data.u_firstname +' '+ data.u_lastname
-                                        }],
-                                        "Subject": otp +" is your verification otp for ratefreelancer",
-                                        "TextPart": "Hi "+data.u_firstname+" "+data.u_lastname+" "+otp+" is your verification otp use this in the next 6 minutes",
-                                        "HTMLPart": "<h1 style='font-color:#343a40;'>Welcome To RateFreelancer</h1><br>your verification otp is <b>"+otp+"</b>, use this otp to verify your email <br><br/> Cheers,<br>Ratefreelancer Team"
-                                    }]
-                                })
-                                request
-                                    .then((result) => {
-                                        // create customer in stripe
-                                        let stripe_customer_id;
-                                        stripe.customers.create({
-                                            description : 'RateFreelancer customer',
-                                            name        : ''+data.u_firstname+' '+data.u_lastname+'',
-                                            email       : data.u_email
-                                        })
-                                        .then(customer => {
-                                            // Update the customer id of the user
-                                            pool.query(
-                                                `UPDATE ka_user SET customer_id = ? WHERE u_email = ?`,
-                                                [
-                                                    customer.id,
-                                                    data.u_email
-                                                ],
-                                                (error,results,fields) => {
-                                                    if(error)
-                                                    {
-                                                        console.log(error);
-                                                    }
-                                                }
-                                            )
-                                        })
-                                        .catch(error => console.error(error));
-                                    })
-                                    .catch((err) => {
-                                        console.log(err);
-                                    })
+                    )   
+                    // generate a token
+                    var secret = speakeasy.generateSecret({length: 20});
+                    var otp = speakeasy.totp({
+                        secret: secret.base32,
+                        encoding: 'base32',
+                        digits:4,
+                        step: 60,
+                        window:10
+                    });
+                    pool.query(
+                        `insert into ka_emailvalidate(u_email,u_otp)
+                        value(?,?)`,
+                        [
+                            data.u_email,
+                            otp
+                        ],
+                        (error,results,fields) => {
+                            if(error)
+                            {
+                                callback(error)
                             }
-                        );
-                        return callback(null,results)
-                    }
-    
+                            // Send email with email template
+                            const request = mailjet
+                            .post("send", {'version': 'v3.1'})
+                            .request({
+                                "Messages":[{
+                                    "From": {
+                                        "Email": "security-noreply@ratefreelancer.com",
+                                        "Name": "Ratefreelancer Security"
+                                    },
+                                    "To": [{
+                                        "Email": data.u_email,
+                                        "Name": data.u_firstname +' '+ data.u_lastname
+                                    }],
+                                    "Subject": otp +" is your verification otp for ratefreelancer",
+                                    "TextPart": "Hi "+data.u_firstname+" "+data.u_lastname+" "+otp+" is your verification otp use this in the next 6 minutes",
+                                    "HTMLPart": "<h1 style='font-color:#343a40;'>Welcome To RateFreelancer</h1><br>your verification otp is <b>"+otp+"</b>, use this otp to verify your email <br><br/> Cheers,<br>Ratefreelancer Team"
+                                }]
+                            })
+                            request
+                                .then((result) => {
+                                    // create customer in stripe
+                                    let stripe_customer_id;
+                                    stripe.customers.create({
+                                        description : 'RateFreelancer customer',
+                                        name        : ''+data.u_firstname+' '+data.u_lastname+'',
+                                        email       : data.u_email
+                                    })
+                                    .then(customer => {
+                                        // Update the customer id of the user
+                                        pool.query(
+                                            `UPDATE ka_user SET customer_id = ? WHERE u_email = ?`,
+                                            [
+                                                customer.id,
+                                                data.u_email
+                                            ],
+                                            (error,results,fields) => {
+                                                if(error)
+                                                {
+                                                    console.log(error);
+                                                }
+                                                return callback(null,results)
+                                            }
+                                        )
+                                    })
+                                    .catch(error => console.error(error));
+                                })
+                                .catch((err) => {
+                                    console.log(err);
+                                })
+                        }
+                    );    
                 }
             );
         }
