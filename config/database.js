541a1233380ed79276aba6436f53063592dbb4cf
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-05-09 11:24:26 +0530

    Update

--- config/database.js
+++ config/database.js
@@ -1,12 +1,11 @@
 const { createPool } = require('mysql');
 
 const pool = createPool({
-    port    :   3306,
-    host    :   process.env.DB_HOST,
-    user    :   process.env.DB_USER,
-    password:   process.env.DB_PASS,
-    database:   process.env.DB_DATABASE,
-    connectionLimit:    10
+    port    :   process.env.RDS_PORT,
+    host    :   process.env.RDS_HOSTNAME,
+    user    :   process.env.RDS_USERNAME,
+    password:   process.env.RDS_PASSWORD,
+    database:   process.env.RDS_DB_NAME
 });
 
 module.exports = pool;
