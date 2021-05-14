fa98cb8e1e9b6e6c5c9335629446c18b7deedf68
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-05-14 11:16:45 +0530

    Update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const mysql = require('mysql');
 const rateLimit = require("express-rate-limit");
 const express = require('express');
-var cors = require('cors');
 const app = express();
 const signupRouter = require("./api/signup/signup.router");
 const loginRouter = require("./api/login/login.router");
@@ -14,9 +13,14 @@ const GetCat = require('./api/cat/cat.router');
 const ReviewRouter = require('./api/review/review.router');
 // SET JSON BODY AS DEFAULT
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONT_END_URL
-}));
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://onelink.cards');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Pass to next layer of middleware
+    next();
+});
 // SET JSON RESPONSE WHEN SOMEONE VISIT THE PAGE
 app.get("/",(req,res) =>{
     res.json({
