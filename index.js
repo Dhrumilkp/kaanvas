a57cd00e79d81c8fa0be28095398f0ffe303257c
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-05-14 11:26:25 +0530

    Update

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'onelink.cards');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Pass to next layer of middleware
