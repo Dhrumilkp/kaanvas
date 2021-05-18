222fdcf3e74d2cda5f1a583f0736a3388d70b942
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-05-18 12:01:02 +0530

    Update

--- api/review/review.service.js
+++ api/review/review.service.js
@@ -73,7 +73,7 @@ module.exports = {
                 {
                     callback(error)
                 }
-                const u_uid = results['id'];
+                const u_uid = results[0]['id'];
                 return callback(null,u_uid);
                 pool.query(
                     `SELECT *,ka_industry_cat.name as industry_cat_name ,ka_sub_cat.name as industry_sub_cat_name FROM ka_collect_url LEFT JOIN ka_industry_cat ON ka_collect_url.industry_cat = ka_industry_cat.id LEFT JOIN ka_sub_cat ON ka_collect_url.industry_sub_cat = ka_sub_cat.id WHERE ka_collect_url.u_uid = ? AND ka_collect_url.is_used = ? ORDER BY ka_collect_url.id`,
