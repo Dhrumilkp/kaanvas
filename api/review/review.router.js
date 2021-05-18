465aac39dfd5d41e43933f79a7802047a2428722
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-05-18 11:37:30 +0530

    Update

--- api/review/review.router.js
+++ api/review/review.router.js
@@ -8,5 +8,5 @@ const {checkToken} = require("../../auth/token_validation");
 
 router.get("/:id",ReviewData);
 router.post("/update/:id",checkToken,UpdateReviewData);
-router.get("/all/:username",checkToken,GetallreviewUser);
+router.get("/all/:username",GetallreviewUser);
 module.exports = router;
