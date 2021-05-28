82da9880ff96daef75e4b6004916aa6fb4fb04cf
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-05-28 13:59:02 +0530

    Update

--- api/sub/sub.controller.js
+++ api/sub/sub.controller.js
@@ -45,10 +45,6 @@ module.exports = {
         )
         .then(
             payment_intent_object => {
-                return res.status(200).json({
-                    status: "success",
-                    message: payment_intent_object
-                });
                 // Update customer payment as default
                 stripe.customers.update(
                     body.customerId,
@@ -65,25 +61,16 @@ module.exports = {
                                 items: [
                                     {price: body.priceId},
                                 ],
+                                payment_behavior: 'default_incomplete',
                                 expand : ['latest_invoice.payment_intent'],
                                 coupon : body.coupon_code
                             })
                             .then(
                                 subscription_object =>{
-                                    subscription_object.u_uid = u_uid;
-                                    subscription_object.price_id_pass = body.priceId;
-                                    createsubscription(subscription_object,(err,results) => {
-                                        if(err)
-                                        {
-                                            return res.status(500).json({
-                                                status: "err",
-                                                message: "Internal server err, please reach out to our support team on support@onelink.cards"
-                                            });
-                                        }
-                                        return res.status(200).json({
-                                            status: "success",
-                                            message: subscription_object
-                                        });
+                                    return res.status(200).json({
+                                        status: "pending_payments",
+                                        subscriptionId: subscription_object.id,
+                                        clientSecret: subscription_object.latest_invoice.payment_intent.client_secret,
                                     });
                                 }
                             )
@@ -101,24 +88,15 @@ module.exports = {
                                 items: [
                                     {price: body.priceId},
                                 ],
+                                payment_behavior: 'default_incomplete',
                                 expand : ['latest_invoice.payment_intent']
                             })
                             .then(
                                 subscription_object =>{
-                                    subscription_object.u_uid = u_uid;
-                                    subscription_object.price_id_pass = body.priceId;
-                                    createsubscription(subscription_object,(err,results) => {
-                                        if(err)
-                                        {
-                                            return res.status(500).json({
-                                                status: "err",
-                                                message: "Internal server err, please reach out to our support team on support@onelink.cards"
-                                            });
-                                        }
-                                        return res.status(200).json({
-                                            status: "success",
-                                            message: subscription_object
-                                        });
+                                    return res.status(200).json({
+                                        status: "pending_payments",
+                                        subscriptionId: subscription_object.id,
+                                        clientSecret: subscription_object.latest_invoice.payment_intent.client_secret,
                                     });
                                 }
                             )
