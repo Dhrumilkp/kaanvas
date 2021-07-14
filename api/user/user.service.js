c8d032de9de57f25b94b1ba0a953d48d3a0072c3
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-07-14 12:41:10 +0530

    Update

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -5,7 +5,7 @@ var speakeasy = require("speakeasy");
 module.exports = {
     getUserByid : (id,callback) => {
         pool.query(
-            `SELECT u_state,u_line2_add,u_line1_add,u_postal_code,u_country_iso,u_firstname,u_lastname,u_email,u_username,onboarding_status,mailverify_status,u_join,u_profilepic_webp,u_profilepic_jpeg,u_coverpic_webp,u_coverpic_jpeg,u_verified,u_country,u_city,is_profile_complete,u_level,is_pro,customer_id,u_website_url,u_dribbble_url,u_github_url,u_default_profile_pic,u_profile_bg_default,u_profile_bg,u_profile_bg_settings,u_profileroot_code FROM ka_user WHERE id = ?`,
+            `SELECT per_hour,u_state,u_line2_add,u_line1_add,u_postal_code,u_country_iso,u_firstname,u_lastname,u_email,u_username,onboarding_status,mailverify_status,u_join,u_profilepic_webp,u_profilepic_jpeg,u_coverpic_webp,u_coverpic_jpeg,u_verified,u_country,u_city,is_profile_complete,u_level,is_pro,customer_id,u_website_url,u_dribbble_url,u_github_url,u_default_profile_pic,u_profile_bg_default,u_profile_bg,u_profile_bg_settings,u_profileroot_code FROM ka_user WHERE id = ?`,
             [
                 id
             ],
@@ -37,8 +37,9 @@ module.exports = {
                 if(!results[0])
                 {
                     pool.query (
-                        `UPDATE ka_user SET u_firstname = ?, u_lastname = ?, u_username = ?,u_state = ?, u_city = ?, u_country = ?, u_country_iso = ?,u_postal_code = ?,u_line1_add = ?, u_line2_add = ? WHERE id = ?`,
+                        `UPDATE ka_user SET per_hour = ?, u_firstname = ?, u_lastname = ?, u_username = ?,u_state = ?, u_city = ?, u_country = ?, u_country_iso = ?,u_postal_code = ?,u_line1_add = ?, u_line2_add = ? WHERE id = ?`,
                         [
+                            data.per_hour,
                             data.u_firstname,
                             data.u_lastname,
                             data.u_username,
@@ -48,30 +49,13 @@ module.exports = {
                             data.u_country_iso,
                             data.u_postal_code,
                             data.u_line1_add,
-                            data.u_line2_add,  
-                            data.u_uid
+                            data.u_line2_add,
+                            data.u_uid,
                         ],
                         (error,results,fields) => {
                             if(error)
                             {
                                 callback(error);
-                            }
-                            if(results)
-                            {
-                                pool.query(
-                                    `UPDATE ka_user SET onboarding_status = ? WHERE id = ?`,
-                                    [
-                                        1,
-                                        data.u_uid
-                                    ],
-                                    (error,results,fields) => {
-                                        console.log(results[0]);
-                                        if(error)
-                                        {
-                                            console.log(error);
-                                        }
-                                    }
-                                )
                             }
                             return callback(null,results);
                         }
@@ -99,7 +83,7 @@ module.exports = {
     },
     CheckForProfile:(data,callback) => {
         pool.query(
-            `SELECT custom_rule,id as u_uid,u_firstname,u_lastname,u_email,u_username,onboarding_status,mailverify_status,u_join,u_profilepic_webp,u_profilepic_jpeg,u_coverpic_webp,u_coverpic_jpeg,u_verified,u_country,u_city,is_profile_complete,u_level,is_pro,customer_id,u_website_url,u_dribbble_url,u_github_url,u_default_profile_pic,u_profile_bg_default,u_profile_bg,u_profile_bg_settings,u_profileroot_code,tag_line FROM ka_user WHERE u_username = ?`,
+            `SELECT per_hour,custom_rule,id as u_uid,u_firstname,u_lastname,u_email,u_username,onboarding_status,mailverify_status,u_join,u_profilepic_webp,u_profilepic_jpeg,u_coverpic_webp,u_coverpic_jpeg,u_verified,u_country,u_city,is_profile_complete,u_level,is_pro,customer_id,u_website_url,u_dribbble_url,u_github_url,u_default_profile_pic,u_profile_bg_default,u_profile_bg,u_profile_bg_settings,u_profileroot_code,tag_line FROM ka_user WHERE u_username = ?`,
             [
                 data.username
             ],
