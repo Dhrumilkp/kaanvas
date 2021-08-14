26d806eb3f2a8ff54b25b07ff5ded29780c3a32f
Dhrumilkp <48042159+Dhrumilkp@users.noreply.github.com>
2021-08-14 10:18:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ const InboxRouter = require('./api/inbox/inbox.router');
 app.use(express.json());
 app.use(function (req, res, next) {
     if (req.hostname.endsWith('onelink.cards')) {
-        res.setHeader('Access-Control-Allow-Origin', 'https://' + req.hostname); 
+        res.setHeader('Access-Control-Allow-Origin', 'http://' + req.hostname);
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     }
-    // Website you wish to allow to connect
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('accept-ranges','bytes');
     res.setHeader('content-type','application/json');
     res.setHeader('referrer-policy','origin-when-cross-origin');
