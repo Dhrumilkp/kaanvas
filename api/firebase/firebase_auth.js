3a77d793209ec980ecb4544c750e3f28e9a69096
Dhrumil Patel <48042159+Dhrumilkp@users.noreply.github.com>
2021-12-28 16:17:21 +0530

    new auth update

---
+++ api/firebase/firebase_auth.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const app = {
+    apiKey: "AIzaSyCzKAhwoNOTNmzZ3i-2fhVjUXzCCSc6_Y0",
+    authDomain: "onelink-cards-backend.firebaseapp.com",
+    projectId: "onelink-cards-backend",
+    storageBucket: "onelink-cards-backend.appspot.com",
+    messagingSenderId: "154501126589",
+    appId: "1:154501126589:web:857159dac383e784705ad4",
+    measurementId: "G-NETGYG5TTT"
+};
+
+if(!firebase.app.length > 0)
+{
+    firebase.initialize.app(app);
+    console.log("Firebase has been initalized");
+}
